1f5629aa3f64e978475292d2fa7d8d5e7aa0b298
guofeng.song 宋国锋 <guofeng.song@qiyuesuo.com>
2017-06-29 14:45:00 +0800

    标准签-添加签署法人章示例

--- src/main/java/com/qiyuesuo/sdk/sample/StandardSignSample.java
+++ src/main/java/com/qiyuesuo/sdk/sample/StandardSignSample.java
@@ -64,6 +64,7 @@ public class StandardSignSample {
 		Receiver platformReceiver = new Receiver();
 		platformReceiver.setType(UserType.PLATFORM);
 		platformReceiver.setOrdinal(1);
+		platformReceiver.setLegalPersonRequired(true);
 		// 签署方，其他需要签署的个人或公司
 		Receiver receiver = new Receiver();
 		receiver.setAuthLevel(AuthLevel.BASIC);
@@ -101,18 +102,25 @@ public class StandardSignSample {
 		Long tempCategoryId = 2284939986421678080l;
 		documentid = standardSignService.create(templateId,templateParams,"测试模版合同",tempCategoryId,receivers,ReceiveType.SEQ);
 		logger.info("标准签使用指定合同分类创建模版合同完成，生成文件id:{}",documentid);
-	
+
 		// ====================================================
-		// 合同签署
+		// 合同法人章签署，法人章签署要在公章签署之前进行
 		// 签署页码和位置
-		Stamper stamper = new Stamper(1, 0.5f, 0.5f);
+		Stamper stamper = new Stamper(1, 0.1f, 0.1f);
+		standardSignService.signByLpseal(documentid, stamper);
+		logger.info("标准签合同法人章签署完成");
+		
+		// ====================================================
+		// 合同公章签署
+		// 签署页码和位置
+		stamper = new Stamper(1, 0.5f, 0.5f);
 		//公章编号，需前往契约锁平台【公章管理】获取
 		Long sealId = 2249772556456296448l;
 		// 附带骑缝章外观的签署
 		standardSignService.sign(documentid, sealId, stamper, true);
 		// 不附带骑缝章外观的签署
 //		standardSignService.sign(documentid, sealId, stamper, false);
-		logger.info("标准签合同签署完成");
+		logger.info("标准签合同公章签署完成");
 		
 		// ====================================================
 		// 获取合同详情
